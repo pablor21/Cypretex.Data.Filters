5390255dd91695f0f246f284456d1c83b2fc9240
Pablo Ramírez <pablor21@gmail.com>
2020-09-12 20:52:50 -0500

    Add support to netstandard

---
+++ tests/MemoryTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace Cypretex.Data.Filters.Tests
+{
+    public class MemoryTests
+    {
+
+        
+
+        [Fact]
+        public void DoTests()
+        {
+
+        }
+    }
+}
---
+++ tests/Models.cs
@@ -0,0 +1,11 @@
+namespace Cypretex.Data.Filters.Tests
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long? AnualSalary { get; set; }
+        public string Phone { get; set; }
+    }
+}
